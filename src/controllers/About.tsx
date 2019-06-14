82475ee7ed5d3e0efd8c911b74ec738788be762c
Roger Qiu <roger.qiu@polyhack.io>
2019-06-14 18:45:18 +1000

    Chnaged name of export for About.tsx

--- src/controllers/About.tsx
+++ src/controllers/About.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Dummies from '../containers/Dummies';
 
-function Home (props) {
+function About (props) {
   return (
     <React.Fragment>
       <Header />
@@ -14,4 +14,4 @@ function Home (props) {
   );
 }
 
-export default Home;
+export default About;
